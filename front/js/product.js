6425cb16d13bf33c3252a849a841b36ed6fd0066
lotfi-elmalemi <elmalemi.lotfi@gmail.com>
2022-08-10 14:52:05 +0200

    description fonction displayProduct

--- front/js/product.js
+++ front/js/product.js
@@ -5,30 +5,30 @@ const host = "http://localhost:3000/";
 const objectURL = host + "api/products/" + id;
 
 
-let cardsFetch = function () {
+let displayProduct = function () {
   fetch(objectURL)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      // get data image
+      // Image et Alttext
       let img = document.querySelector(".item__img");
       img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-      // data.name and title
+      // Nom et titre
       let name = document.getElementById("title");
       name.innerHTML = data.name;
       let title = document.querySelector("title");
       title.innerHTML = data.name;
-      // price
+      // prix
       let price = document.getElementById("price");
       price.innerHTML = `${data.price}`;
       // description
       let description = document.getElementById("description");
       description.innerHTML = data.description;
-      // colors
+      // couleurs
       let color = document.getElementById("colors");
       for (i = 0; i < data.colors.length; i++) {
         color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
       }
     });
 };
-cardsFetch();
+displayProduct();
